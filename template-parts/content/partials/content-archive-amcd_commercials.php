4874a735d98539cac2222e71cb215de0699863d8
ControlledChaos <greg@ccdzine.com>
2018-10-11 12:14:08 -0800

    Added feature to fetch Vineo data from URL fro screenshot and Fancybox.

--- template-parts/content/partials/content-archive-amcd_commercials.php
+++ template-parts/content/partials/content-archive-amcd_commercials.php
@@ -16,16 +16,31 @@ if ( have_posts() ) : ?>
 <article class="global-wrapper" role="index">
     <ul class="video-grid commercials"><?php while ( have_posts() ) : the_post();
 
-    $client   = get_field( 'amcd_commercial_client' );
-    $title    = get_field( 'amcd_commercial_title' );
-    $director = get_field( 'amcd_commercial_director' );
-    $image    = get_field( 'amcd_commercial_thumbnail' );
-    $vimeo    = get_field( 'amcd_commercial_vimeo_id' );
-    $size     = 'video-medium';
-    $thumb    = $image['sizes'][ $size ];
-    $srcset   = wp_get_attachment_image_srcset( $image['ID'], $size );
-    $width    = $image['sizes'][ $size . '-width' ];
-    $height   = $image['sizes'][ $size . '-height' ];
+    $client     = get_field( 'amcd_commercial_client' );
+    $title      = get_field( 'amcd_commercial_title' );
+    $director   = get_field( 'amcd_commercial_director' );
+    $image      = get_field( 'amcd_commercial_thumbnail' );
+    $vimeo      = get_field( 'amcd_commercial_vimeo_id' );
+    $size       = 'video-medium';
+    $thumb      = $image['sizes'][ $size ];
+    $srcset     = wp_get_attachment_image_srcset( $image['ID'], $size );
+    $width      = $image['sizes'][ $size . '-width' ];
+    $height     = $image['sizes'][ $size . '-height' ];
+    $vimeo_data = json_decode( file_get_contents( 'http://vimeo.com/api/oembed.json?url=' . $vimeo ) );
+
+    if ( $image ) {
+        $thumb = $image['sizes'][ $size ];
+    } elseif ( $vimeo_data ) {
+        $thumb = $vimeo_data->thumbnail_url;
+    } else {
+        $thumb = get_parent_theme_file_uri( '/assets/images/video-placeholder.jpg' );
+    }
+
+    if ( ! $vimeo_data ) {
+        $vimeo = null;
+    } else {
+        $vimeo = $vimeo_data->video_id;
+    }
 
     if ( $title && $director ) {
         $caption = $client . '<br />' . $title . '<br />Dirercted by ' . $director;
